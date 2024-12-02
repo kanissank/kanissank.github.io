b73fd81d288d13ba9189b1a1d6de365a0e571845
kanissank <kanissboss@gmail.com>
2024-12-02 16:34:16 -0500

    Update main.js

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -34,11 +34,14 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300 / 14);
-    const temperature =  Math.round((94 - 32) * (5/9));
+    const weight = `${Math.round(300 / 14)}stone`;
+    const temperature =  `${Math.round((94 - 32) * (5/9))} centigrade`;
+    newStory = newStory
+                .replace("300 pounds", weight)
+                .replace("94 fahrenheit", temperature);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
