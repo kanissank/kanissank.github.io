967bf828975b2ada5fd22a8cdf3c40e59fb51bbe
kanissank <kanissboss@gmail.com>
2024-12-02 22:36:55 -0500

    added comments

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -1,31 +1,47 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
+
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgArr = ["pic1.jpg", "pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
 
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white pansies',
+  'pic4.jpg': 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg': 'Large moth on a leaf',
+};
 /* Declaring the alternative text for each image file */
-const imgAlts = [
-    {altText: "photo 1 - picture of a eye"},
-    {altText: "photo 2 - picture of brown rock"},
-    {altText: "photo 3 - picture of purple and white flowers"},
-    {altText: "photo 4 - picture of the first nations"},
-    {altText: "photo 5 - picture of a butterfly"},
-   
-]
 
-/* Looping through images */\
-for (let i =0; i <imgArr.length; i++) {
-    const newImage = document.createElement("img");
-    newImage.setAttribute('src', `images/${imgArr[i]}`);
-    newImage.setAttribute('alt', imgAlts[i].altText);
-    thumbBar.appendChild(newImage);
-
-    
+/* Looping through images */
+for (const image of images) {
+const newImage = document.createElement('img');
+newImage.setAttribute('src', `images/${image}`);
+newImage.setAttribute('alt', alts[image]);
+thumbBar.appendChild(newImage);
+newImage.addEventListener('click', e => {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+  });
 }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
+
 
 
