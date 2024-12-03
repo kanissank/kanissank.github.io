89ddbc445ecd2116ce756cb1687fc3bb6bf2e95a
kanissank <kanissboss@gmail.com>
2024-12-02 22:10:44 -0500

    added
    all pictures
    .setattribute
    and added description

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -1,15 +1,27 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgArr = [pic1.jgp]
+const imgArr = ["pic1.jpg", "pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const imgAlts = [
+    {altText: "photo 1 - picture of a eye"},
+    {altText: "photo 2 - picture of brown rock"},
+    {altText: "photo 3 - picture of purple and white flowers"},
+    {altText: "photo 4 - picture of the first nations"},
+    {altText: "photo 5 - picture of a butterfly"},
+   
+]
 
-/* Looping through images */
+/* Looping through images */\
+for (let i =0; i <imgArr.length; i++) {
+    const newImage = document.createElement("img");
+    newImage.setAttribute('src', `images/${imgArr[i]}`);
+    
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
